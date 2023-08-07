0c8a73d93a30e205b454152d517a9fe46d26beda
YannickGir <girofare@hotmail.com>
2023-08-07 11:38:12 +0200

    new project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+<div className='header'>
+header
+
+      </div>
+      <div className='movies'>
+movies
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
