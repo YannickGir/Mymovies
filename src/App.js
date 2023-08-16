7abf4f2b30aa85935b19085afe946afdee5a6ed0
YannickGir <girofare@hotmail.com>
2023-08-16 22:47:53 +0200

    add rating movie

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card,CardBody, CardTitle, CardSubtitle,CardText, Nav, NavItem, NavLink, Button, ButtonGroup } from 'reactstrap';
+import {Card,CardBody, CardTitle, CardSubtitle,CardText, Nav, NavItem, NavLink, Button, ButtonGroup, Badge } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faVideo, faStar, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
+  const[likeMovie, setLikeMovie] = useState(false)
+  const[watchMovie, setWatchMovie] = useState(false)
+  const[countWatchMovie, setcountWatchMovie] = useState(0)
+  const [myRatingMovie, setMyRatingMovie] = useState(0)
+
+  function lowratingmovie () {
+    if (myRatingMovie > 0) {
+      setMyRatingMovie(myRatingMovie-1)
+    }
+  }
+
+    if (likeMovie == false)
+    var colorLike = {} 
+    else colorLike ='#e74c3c'
+ 
+    if (watchMovie == false)
+    var colorMovie = {}
+    else colorMovie = '#e74c3c'
+  
+  console.log(likeMovie);
+  console.log(watchMovie);
   return (
     <div className="App">
+      <div className="wrapper">
 <div className='header'>
 <Nav
 >
   <span>
-    <img src='./logo_movie.png' width={40} height={40}/>
+    <img src='./logo_movie.png' width={50} height={50}/>
   </span>
   <NavItem>
     <NavLink>Last Releases</NavLink>
@@ -34,10 +57,62 @@ function App() {
   />
   <CardBody >
   <CardText>
-      Like <FontAwesomeIcon icon={faHeart} className='pointer'/>
+      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
     </CardText>
     <CardText>
-      Nombre de vues <FontAwesomeIcon icon={faVideo} />
+      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
+    </CardText>
+    <CardText>
+      Mon avis 
+      <FontAwesomeIcon icon={faStar} /> 
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <ButtonGroup size='sm'> 
+      <Button onClick={()=>{lowratingmovie()}}>-</Button> 
+      <Button onClick={()=>{setMyRatingMovie(myRatingMovie+1)}}>+</Button> {myRatingMovie}
+      </ButtonGroup>
+    </CardText>
+    <CardText>
+      Moyenne 
+      <FontAwesomeIcon icon={faStar} /> 
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      (8)
+    </CardText>
+    <CardTitle tag="h5">
+      Film title
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+      Type of Movie
+    </CardSubtitle>
+    <CardText>
+      Movie description
+    </CardText>
+  </CardBody>
+</Card>
+<Card className='card-item' 
+>
+  <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+  <CardBody >
+  <CardText>
+      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
+    </CardText>
+    <CardText>
+      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
     </CardText>
     <CardText>
       Mon avis 
@@ -86,62 +161,10 @@ function App() {
   />
   <CardBody >
   <CardText>
-      Like <FontAwesomeIcon icon={faHeart} className='pointer'/>
+      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
     </CardText>
     <CardText>
-      Nombre de vues <FontAwesomeIcon icon={faVideo} />
-    </CardText>
-    <CardText>
-      Mon avis 
-      <FontAwesomeIcon icon={faStar} /> 
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <ButtonGroup size='sm'> 
-      <Button>-</Button> 
-      <Button>+</Button>
-      </ButtonGroup>
-    </CardText>
-    <CardText>
-      Moyenne 
-      <FontAwesomeIcon icon={faStar} /> 
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      (8)
-    </CardText>
-    <CardTitle tag="h5">
-      Film title
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      Type of Movie
-    </CardSubtitle>
-    <CardText>
-      Movie description
-    </CardText>
-  </CardBody>
-</Card>
-<Card className='card-item' 
->
-  <img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody >
-  <CardText>
-      Like <FontAwesomeIcon icon={faHeart} className='pointer' />
-    </CardText>
-    <CardText>
-      Nombre de vues <FontAwesomeIcon icon={faVideo} />
+      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
     </CardText>
     <CardText>
       Mon avis 
@@ -183,7 +206,163 @@ function App() {
     </CardText>
   </CardBody>
 </Card>
-
+<Card className='card-item' 
+>
+  <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+  <CardBody >
+  <CardText>
+      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
+    </CardText>
+    <CardText>
+      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
+    </CardText>
+    <CardText>
+      Mon avis 
+      <FontAwesomeIcon icon={faStar} /> 
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <ButtonGroup size='sm'> 
+      <Button>-</Button> 
+      <Button>+</Button>
+      </ButtonGroup>
+    </CardText>
+    <CardText>
+      Moyenne 
+      <FontAwesomeIcon icon={faStar} /> 
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      (8)
+    </CardText>
+    <CardTitle tag="h5">
+      Film title
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+      Type of Movie
+    </CardSubtitle>
+    <CardText>
+      Movie description
+    </CardText>
+  </CardBody>
+</Card>
+<Card className='card-item' 
+>
+  <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+  <CardBody >
+  <CardText>
+      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
+    </CardText>
+    <CardText>
+      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
+    </CardText>
+    <CardText>
+      Mon avis 
+      <FontAwesomeIcon icon={faStar} /> 
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <ButtonGroup size='sm'> 
+      <Button>-</Button> 
+      <Button>+</Button>
+      </ButtonGroup>
+    </CardText>
+    <CardText>
+      Moyenne 
+      <FontAwesomeIcon icon={faStar} /> 
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      (8)
+    </CardText>
+    <CardTitle tag="h5">
+      Film title
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+      Type of Movie
+    </CardSubtitle>
+    <CardText>
+      Movie description
+    </CardText>
+  </CardBody>
+</Card>
+<Card className='card-item' 
+>
+  <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+  <CardBody >
+  <CardText>
+      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
+    </CardText>
+    <CardText>
+      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
+    </CardText>
+    <CardText>
+      Mon avis 
+      <FontAwesomeIcon icon={faStar} /> 
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <ButtonGroup size='sm'> 
+      <Button>-</Button> 
+      <Button>+</Button>
+      </ButtonGroup>
+    </CardText>
+    <CardText>
+      Moyenne 
+      <FontAwesomeIcon icon={faStar} /> 
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      <FontAwesomeIcon icon={faStar} />
+      (8)
+    </CardText>
+    <CardTitle tag="h5">
+      Film title
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+      Type of Movie
+    </CardSubtitle>
+    <CardText>
+      Movie description
+    </CardText>
+  </CardBody>
+</Card>
+      </div>
       </div>
     </div>
   );
