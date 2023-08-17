05b8da39a09b18526cca1b4981323aab93896d7e
YannickGir <girofare@hotmail.com>
2023-08-17 13:15:41 +0200

    create movie component and dynamic display with a custom datas

--- src/App.js
+++ src/App.js
@@ -1,95 +1,51 @@
 import './App.css';
-import { useState, useEffect, startTransition } from 'react';
+import { useState, useEffect, startTransition, Row } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card,CardBody, CardTitle, CardSubtitle,CardText, Nav, NavItem, NavLink, Button, ButtonGroup, Badge } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faVideo, faStar, faHeart, faDisplay } from '@fortawesome/free-solid-svg-icons'
+import {faVideo, faStar, faHeart, faDisplay } from '@fortawesome/free-solid-svg-icons';
+import Movie from './Movie';
 
 
 function App() {
-  const[likeMovie, setLikeMovie] = useState(false)
-  const[watchMovie, setWatchMovie] = useState(false)
-  const[countWatchMovie, setcountWatchMovie] = useState(0)
-  const [myRatingMovie, setMyRatingMovie] = useState(0)
-  const [myRatingStar, setMyRatingStar] = useState(null)
-  const [hover, setHover] = useState(null)
-  const [randomRating, setRandomRating] = useState(8)
-  const [currentRating, setCurrentRating] = useState(null)
-    const [numberofrates, setNumberofrates] = useState(2)
-    const [newNumberofrates, setNewNumberofrates] = useState(numberofrates)
 
-
-
-   const stars = [...Array(10)].map((_,index) => 
-    { 
-      const currentRatingFromStars = index + 1;
-        return (
-      <label key={currentRatingFromStars}>
-        <input 
-        style={{display:'none'}}
-        type='radio' 
-        name='rating' 
-        value={currentRatingFromStars} 
-        onClick={()=>{
-          setMyRatingStar(currentRatingFromStars); 
-          setNewNumberofrates(numberofrates + 1)}} 
-        />
-        <FontAwesomeIcon 
-        color={currentRatingFromStars <= (hover || myRatingStar) ? '#f1c40f' : "black"} 
-        icon={faStar} 
-        onMouseEnter={()=>setHover(currentRatingFromStars)} 
-        onMouseLeave={()=>setHover(null)}
-        />
-      </label>
-      )
-    }
-  );
-
-  useEffect(() => {
-    if (myRatingStar !== null) {
-      const newRandomRating =
-        (randomRating * numberofrates + myRatingStar) / newNumberofrates ;
-      setRandomRating(Math.round(newRandomRating));
-    }
-    
-  }, [numberofrates, newNumberofrates, myRatingStar, randomRating]);
-
-  const randomstars = [...Array(10)].map((_,index) => 
-  { 
-      return (
-    <label>
-      <FontAwesomeIcon 
-      color={randomRating <= 10 ? '#f1c40f' : "black"} 
-      icon={faStar} 
-      value={randomRating} 
-      />
-    </label>
-    )
-  }
-);
-
-  function addratingmovie () {
-    if (myRatingMovie < 10) {
-      setMyRatingMovie(myRatingMovie+1)
-    }
-  }
-
-    if (likeMovie == false)
-    var colorLike = {} 
-    else colorLike ='#e74c3c'
- 
-    if (watchMovie == false)
-    var colorMovie = {}
-    else colorMovie = '#e74c3c'
+const movieDatas = [
+{title: "La guerre des étoiles", typeOfMovie: "Science Fiction", description: "Les méchants attaquent les gentils dans les étoiles", affiche:"https://www.bedetheque.com/media/Couvertures/Couv_47062.jpg", rating:7, ratingCount:10 },
+{title: "Le Seigneur des Anneaux - Le Retour du roi", typeOfMovie: "Aventure, Fantasy", description: "C'est l'histoire d'un mec qui était à un doigt de garder l'anneau.", affiche:"https://media.senscritique.com/media/000007087660/0/le_seigneur_des_anneaux_le_retour_du_roi.jpg", rating:5, ratingCount:20 },
+{title: " Her", typeOfMovie: "Drame, Romance, Science-fiction", description: "Un moustachu qui fout des vents à Olivia Wilde, Amy Adams et Scarlett Johansson et qui chiale parce qu'à la fin il est tout seul.", affiche:"https://media.senscritique.com/media/000006629445/0/her.jpg", rating:8, ratingCount:15 },
+{title: " Locke", typeOfMovie: "Drame, Thriller", description: "Jusqu'où est prêt à aller un homme pour être certain que son kit main libre fonctionne parfaitement?", affiche:"https://media.senscritique.com/media/000007088125/0/locke.jpg", rating:7, ratingCount:25 },
+{title: "Alien³", typeOfMovie: "Science-fiction, Épouvante-Horreur", description: "L'héroine se fait poursuivre par un handicapé en roller.", affiche:"https://media.senscritique.com/media/000008632186/0/alien3.jpg", rating:3, ratingCount:30 },
+{title: "The Mist", typeOfMovie: "Épouvante-Horreur, Science-fiction, Thriller", description: "C'est l'histoire d'une toute petite erreur de timing...", affiche:"https://media.senscritique.com/media/000017260476/0/the_mist.jpg", rating:6, ratingCount:40 },
+{title: "Le Prestige ", typeOfMovie: "Drame, Thriller", description: "Une battle de magie entre Wolverine et Batman avec David Bowie, forcément c'est épique.", affiche:"https://media.senscritique.com/media/000004699879/0/le_prestige.jpg", rating:7, ratingCount:12 },
+{title: "American History X", typeOfMovie: "Policier, Drame", description: "Un néonazi qui a décidé de voter à gauche parce qu'il en a eu plein le cul.", affiche:"https://media.senscritique.com/media/000012223258/0/american_history_x.jpg", rating:9, ratingCount:5 },
+{title: "Taken", typeOfMovie: "Action, Thriller", description: "Un homme dont la fille se fait enlever qu'il ne fallait pas énerver car il va chercher, trouver puis tuer à l'aide d'un homme qui va lire : bon chance, sur un bout de papier donc automatiquement c'est lui le kidnappeur.", affiche:"https://media.senscritique.com/media/000000097457/0/taken.jpg", rating:4, ratingCount:17 },
+{title: " American Sniper", typeOfMovie: "Biopic, Guerre", description: "Un mec qui quand il tire, ne raconte pas sa vie.", affiche:"https://media.senscritique.com/media/000020120544/0/american_sniper.jpg", rating:3, ratingCount:32 }
+]
   
-  console.log(likeMovie);
-  console.log(watchMovie);
+const [movieList, setMovieList] = useState([]);
+useEffect(() => {
+  const movies = movieDatas.map((movie, i) => {
+    return (
+      <Movie
+        key={i}
+        myTitle={movie.title}
+        myType={movie.typeOfMovie}
+        myDescription={movie.description}
+        myAffiche={movie.affiche}
+        note={movie.rating}
+        nbVotes={movie.ratingCount}
+      />
+    );
+  });
+  setMovieList(movies);
+}, []);
 
 
   return (
-    <div className="App">
-      <div className="wrapper">
-<div className='header'>
+    
+ <div className="App">
+    <div className="wrapper">
+<div className='header'> 
 <Nav
 >
   <span>
@@ -104,280 +60,15 @@ function App() {
     </NavLink>
   </NavItem>
 </Nav>
-
-      </div>
-      <div className='movies'>
-      <Card className='card-item' 
->
-  <img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody >
-  <CardText>
-      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
-    </CardText>
-    <CardText>
-      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
-    </CardText>
-    <CardText>
-      Mon avis 
-      {stars}
+<div className='movies'>  
+{movieList}
+</div>
  
-    </CardText>
-    <CardText>
-      Moyenne 
-      {randomstars}
-      ({randomRating}) {newNumberofrates}
-    </CardText>
-    <CardTitle tag="h5">
-      Film title
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      Type of Movie
-    </CardSubtitle>
-    <CardText>
-      Movie description
-    </CardText>
-  </CardBody>
-</Card>
-<Card className='card-item' 
->
-  <img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody >
-  <CardText>
-      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
-    </CardText>
-    <CardText>
-      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
-    </CardText>
-    <CardText>
-      Mon avis 
-      {stars}
-      <ButtonGroup size='sm'> 
-      <Button>-</Button> 
-      <Button>+</Button>
-      </ButtonGroup>
-    </CardText>
-    <CardText>
-      Moyenne 
-      <FontAwesomeIcon icon={faStar} /> 
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      (8)
-    </CardText>
-    <CardTitle tag="h5">
-      Film title
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      Type of Movie
-    </CardSubtitle>
-    <CardText>
-      Movie description
-    </CardText>
-  </CardBody>
-</Card>
-<Card className='card-item' 
->
-  <img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody >
-  <CardText>
-      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
-    </CardText>
-    <CardText>
-      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
-    </CardText>
-    <CardText>
-      Mon avis 
-      {stars}
-      <ButtonGroup size='sm'> 
-      <Button>-</Button> 
-      <Button>+</Button>
-      </ButtonGroup>
-     
-    </CardText>
-    <CardText>
-      Moyenne 
-      <FontAwesomeIcon icon={faStar} /> 
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      (8)
-    </CardText>
-    <CardTitle tag="h5">
-      Film title
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      Type of Movie
-    </CardSubtitle>
-    <CardText>
-      Movie description
-    </CardText>
-  </CardBody>
-</Card>
-<Card className='card-item' 
->
-  <img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody >
-  <CardText>
-      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
-    </CardText>
-    <CardText>
-      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
-    </CardText>
-    <CardText>
-      Mon avis 
-      {stars}
-      <ButtonGroup size='sm'> 
-      <Button>-</Button> 
-      <Button>+</Button>
-      </ButtonGroup>
-    </CardText>
-    <CardText>
-      Moyenne 
-      <FontAwesomeIcon icon={faStar} /> 
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      (8)
-    </CardText>
-    <CardTitle tag="h5">
-      Film title
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      Type of Movie
-    </CardSubtitle>
-    <CardText>
-      Movie description
-    </CardText>
-  </CardBody>
-</Card>
-<Card className='card-item' 
->
-  <img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody >
-  <CardText>
-      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
-    </CardText>
-    <CardText>
-      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
-    </CardText>
-    <CardText>
-      Mon avis 
-      {stars}
-      <ButtonGroup size='sm'> 
-      <Button>-</Button> 
-      <Button>+</Button>
-      </ButtonGroup>
-    </CardText>
-    <CardText>
-      Moyenne 
-      <FontAwesomeIcon icon={faStar} /> 
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      (8)
-    </CardText>
-    <CardTitle tag="h5">
-      Film title
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      Type of Movie
-    </CardSubtitle>
-    <CardText>
-      Movie description
-    </CardText>
-  </CardBody>
-</Card>
-<Card className='card-item' 
->
-  <img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody >
-  <CardText>
-      Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
-    </CardText>
-    <CardText>
-      Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
-    </CardText>
-    <CardText>
-      Mon avis 
-      {stars}
-      <ButtonGroup size='sm'> 
-      <Button>-</Button> 
-      <Button>+</Button>
-      </ButtonGroup>
-    </CardText>
-    <CardText>
-      Moyenne 
-      <FontAwesomeIcon icon={faStar} /> 
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      (8)
-    </CardText>
-    <CardTitle tag="h5">
-      Film title
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      Type of Movie
-    </CardSubtitle>
-    <CardText>
-      Movie description
-    </CardText>
-  </CardBody>
-</Card>
+
+    
       </div>
-      </div>
-    </div>
+      </div> 
+</div>
   );
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,126 @@
+import './App.css';
+import { useState, useEffect, startTransition } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card,CardBody, CardTitle, CardSubtitle,CardText, Nav, NavItem, NavLink, Button, ButtonGroup, Badge } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faVideo, faStar, faHeart, faDisplay } from '@fortawesome/free-solid-svg-icons'
+
+function Movie(props) {
+    const[likeMovie, setLikeMovie] = useState(false)
+    const[watchMovie, setWatchMovie] = useState(false)
+    const[countWatchMovie, setcountWatchMovie] = useState(0)
+    const [myRatingMovie, setMyRatingMovie] = useState(0)
+    const [myRatingStar, setMyRatingStar] = useState(null)
+    const [hover, setHover] = useState(null)
+    const [averageRating, setAverageRating] = useState(props.note)
+    const [currentRating, setCurrentRating] = useState(null)
+      const [numberofrates, setNumberofrates] = useState(props.nbVotes)
+      const [newNumberofrates, setNewNumberofrates] = useState(numberofrates)
+  
+  
+  
+     const stars = [...Array(10)].map((_,index) => 
+      { 
+        const currentRatingFromStars = index + 1;
+          return (
+        <label key={currentRatingFromStars}>
+          <input 
+          style={{display:'none'}}
+          type='radio' 
+          name='rating' 
+          value={currentRatingFromStars} 
+          onClick={()=>{
+            setMyRatingStar(currentRatingFromStars); 
+            setNewNumberofrates(numberofrates + 1)}} 
+          />
+          <FontAwesomeIcon 
+          color={currentRatingFromStars <= (hover || myRatingStar) ? '#f1c40f' : "black"} 
+          icon={faStar} 
+          onMouseEnter={()=>setHover(currentRatingFromStars)} 
+          onMouseLeave={()=>setHover(null)}
+          />
+        </label>
+        )
+      }
+    );
+  
+    useEffect(() => {
+      if (myRatingStar !== null) {
+        const newAverageRating =
+          (averageRating * numberofrates + myRatingStar) / newNumberofrates ;
+        setAverageRating(Math.round(newAverageRating));
+      }
+      
+    }, [numberofrates, newNumberofrates, myRatingStar, averageRating]);
+  
+    const averageStars = [...Array(10)].map((_,index) => 
+    { 
+        return (
+      <label>
+        <FontAwesomeIcon 
+        color={averageRating <= 10 ? '#f1c40f' : "black"} 
+        icon={faStar} 
+        value={averageRating} 
+        />
+      </label>
+      )
+    }
+  );
+  
+      if (likeMovie == false)
+      var colorLike = {} 
+      else colorLike ='#e74c3c'
+   
+      if (watchMovie == false)
+      var colorMovie = {}
+      else colorMovie = '#e74c3c'
+    
+    console.log(likeMovie);
+    console.log(watchMovie);
+  
+    return (
+        
+        <Card className='card-item' 
+  >
+    <img
+      alt="Sample"
+      src={props.myAffiche}
+    />
+    <CardBody >
+    <CardText>
+      
+        Like <FontAwesomeIcon onClick={()=>{setLikeMovie(!likeMovie)}} color={colorLike} icon={faHeart} className='pointer'/>
+      </CardText>
+      <CardText>
+        Nombre de vues <FontAwesomeIcon color={colorMovie} icon={faVideo} onClick={()=>{setWatchMovie(!watchMovie); setcountWatchMovie(countWatchMovie + 1)}}/> <Badge>{countWatchMovie}</Badge>
+      </CardText>
+      <CardText>
+        Mon avis 
+        {stars}
+   
+      </CardText>
+      <CardText>
+        Moyenne 
+        {averageStars}
+        ({averageRating}) {newNumberofrates}
+      </CardText>
+      <CardTitle tag="h5">
+        {props.myTitle}
+      </CardTitle>
+      <CardSubtitle
+        className="mb-2 text-muted"
+        tag="h6"
+      >
+        {props.myType}
+      </CardSubtitle>
+      <CardText>
+      {props.myDescription}
+      </CardText>
+    </CardBody>
+  </Card>
+        
+    )
+
+    }
+
+    export default Movie;
