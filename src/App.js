563e8f250ec87941641f77e1fdfe1151d9a2a638
YannickGir <girofare@hotmail.com>
2023-08-21 19:49:49 +0200

    added dynamic  wishlist when click on heart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState, useEffect, startTransition, Row } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card,CardBody, CardTitle, CardSubtitle,CardText, Nav, NavItem, NavLink,  ButtonGroup, Badge } from 'reactstrap';
+import {ListGroup, ListGroupItemText, ListGroupItem, Card,CardBody, CardTitle, CardSubtitle,CardText, Nav, NavItem, NavLink,  ButtonGroup, Badge } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faVideo, faStar, faHeart, faDisplay } from '@fortawesome/free-solid-svg-icons';
 import Movie from './Movie';
@@ -11,16 +11,16 @@ import { Button, Popover, PopoverHeader, PopoverBody} from "reactstrap"
 function App() {
 
 const movieDatas = [
-{title: "La guerre des étoiles", typeOfMovie: "Science Fiction", description: "Les méchants attaquent les gentils dans les étoiles", affiche:"https://www.bedetheque.com/media/Couvertures/Couv_47062.jpg", rating:7, ratingCount:10 },
-{title: "Le Seigneur des Anneaux - Le Retour du roi", typeOfMovie: "Aventure, Fantasy", description: "C'est l'histoire d'un mec qui était à un doigt de garder l'anneau.", affiche:"https://media.senscritique.com/media/000007087660/0/le_seigneur_des_anneaux_le_retour_du_roi.jpg", rating:5, ratingCount:20 },
-{title: " Her", typeOfMovie: "Drame, Romance, Science-fiction", description: "Un moustachu qui fout des vents à Olivia Wilde, Amy Adams et Scarlett Johansson et qui chiale parce qu'à la fin il est tout seul.", affiche:"https://media.senscritique.com/media/000006629445/0/her.jpg", rating:8, ratingCount:15 },
-{title: " Locke", typeOfMovie: "Drame, Thriller", description: "Jusqu'où est prêt à aller un homme pour être certain que son kit main libre fonctionne parfaitement?", affiche:"https://media.senscritique.com/media/000007088125/0/locke.jpg", rating:7, ratingCount:25 },
-{title: "Alien³", typeOfMovie: "Science-fiction, Épouvante-Horreur", description: "L'héroine se fait poursuivre par un handicapé en roller.", affiche:"https://media.senscritique.com/media/000008632186/0/alien3.jpg", rating:3, ratingCount:30 },
-{title: "The Mist", typeOfMovie: "Épouvante-Horreur, Science-fiction, Thriller", description: "C'est l'histoire d'une toute petite erreur de timing...", affiche:"https://media.senscritique.com/media/000017260476/0/the_mist.jpg", rating:6, ratingCount:40 },
-{title: "Le Prestige ", typeOfMovie: "Drame, Thriller", description: "Une battle de magie entre Wolverine et Batman avec David Bowie, forcément c'est épique.", affiche:"https://media.senscritique.com/media/000004699879/0/le_prestige.jpg", rating:7, ratingCount:12 },
-{title: "American History X", typeOfMovie: "Policier, Drame", description: "Un néonazi qui a décidé de voter à gauche parce qu'il en a eu plein le cul.", affiche:"https://media.senscritique.com/media/000012223258/0/american_history_x.jpg", rating:9, ratingCount:5 },
-{title: "Taken", typeOfMovie: "Action, Thriller", description: "Un homme dont la fille se fait enlever qu'il ne fallait pas énerver car il va chercher, trouver puis tuer à l'aide d'un homme qui va lire : bon chance, sur un bout de papier donc automatiquement c'est lui le kidnappeur.", affiche:"https://media.senscritique.com/media/000000097457/0/taken.jpg", rating:4, ratingCount:17 },
-{title: " American Sniper", typeOfMovie: "Biopic, Guerre", description: "Un mec qui quand il tire, ne raconte pas sa vie.", affiche:"https://media.senscritique.com/media/000020120544/0/american_sniper.jpg", rating:3, ratingCount:32 }
+{id:1, title: "La guerre des étoiles", typeOfMovie: "Science Fiction", description: "Les méchants attaquent les gentils dans les étoiles", affiche:"https://www.bedetheque.com/media/Couvertures/Couv_47062.jpg", rating:7, ratingCount:10 },
+{id:2, title: "Le Seigneur des Anneaux - Le Retour du roi", typeOfMovie: "Aventure, Fantasy", description: "C'est l'histoire d'un mec qui était à un doigt de garder l'anneau.", affiche:"https://media.senscritique.com/media/000007087660/0/le_seigneur_des_anneaux_le_retour_du_roi.jpg", rating:5, ratingCount:20 },
+{id:3, title: " Her", typeOfMovie: "Drame, Romance, Science-fiction", description: "Un moustachu qui fout des vents à Olivia Wilde, Amy Adams et Scarlett Johansson et qui chiale parce qu'à la fin il est tout seul.", affiche:"https://media.senscritique.com/media/000006629445/0/her.jpg", rating:8, ratingCount:15 },
+{id:4, title: " Locke", typeOfMovie: "Drame, Thriller", description: "Jusqu'où est prêt à aller un homme pour être certain que son kit main libre fonctionne parfaitement?", affiche:"https://media.senscritique.com/media/000007088125/0/locke.jpg", rating:7, ratingCount:25 },
+{id:5, title: "Alien³", typeOfMovie: "Science-fiction, Épouvante-Horreur", description: "L'héroine se fait poursuivre par un handicapé en roller.", affiche:"https://media.senscritique.com/media/000008632186/0/alien3.jpg", rating:3, ratingCount:30 },
+{id:6, title: "The Mist", typeOfMovie: "Épouvante-Horreur, Science-fiction, Thriller", description: "C'est l'histoire d'une toute petite erreur de timing...", affiche:"https://media.senscritique.com/media/000017260476/0/the_mist.jpg", rating:6, ratingCount:40 },
+{id:7, title: "Le Prestige ", typeOfMovie: "Drame, Thriller", description: "Une battle de magie entre Wolverine et Batman avec David Bowie, forcément c'est épique.", affiche:"https://media.senscritique.com/media/000004699879/0/le_prestige.jpg", rating:7, ratingCount:12 },
+{id:8, title: "American History X", typeOfMovie: "Policier, Drame", description: "Un néonazi qui a décidé de voter à gauche parce qu'il en a eu plein le cul.", affiche:"https://media.senscritique.com/media/000012223258/0/american_history_x.jpg", rating:9, ratingCount:5 },
+{id:9, title: "Taken", typeOfMovie: "Action, Thriller", description: "Un homme dont la fille se fait enlever qu'il ne fallait pas énerver car il va chercher, trouver puis tuer à l'aide d'un homme qui va lire : bon chance, sur un bout de papier donc automatiquement c'est lui le kidnappeur.", affiche:"https://media.senscritique.com/media/000000097457/0/taken.jpg", rating:4, ratingCount:17 },
+{id:10, title: " American Sniper", typeOfMovie: "Biopic, Guerre", description: "Un mec qui quand il tire, ne raconte pas sa vie.", affiche:"https://media.senscritique.com/media/000020120544/0/american_sniper.jpg", rating:3, ratingCount:32 }
 ]
 
 const[likeMovie, setLikeMovie] = useState([])
@@ -39,10 +39,10 @@ console.log({likeMovie});
 }
 
 useEffect(() => {
-  const movies = movieDatas.map((movie, i) => {
+  const movies = movieDatas.map(movie => {
     return (
       <Movie
-        key={i}
+        key={movie.id}
         myTitle={movie.title}
         myType={movie.typeOfMovie}
         myDescription={movie.description}
@@ -51,10 +51,21 @@ useEffect(() => {
         nbVotes={movie.ratingCount}
         handleClickParent={handleClickAddMovie}
       />
-    );
-  });
+    )
+  })
   setMovieList(movies);
 }, [likeMovie]);
+
+
+var cardWish = likeMovie.map((movie, i) => (
+   <ListGroup key={i}>
+  <ListGroupItem >
+  {movie}
+  {console.log(movie)}
+</ListGroupItem>
+</ListGroup>
+ ));
+
 
 
 
@@ -82,17 +93,18 @@ useEffect(() => {
     toggle={toogle}
   >
     <PopoverHeader>
-      Popover Title
+      Liste des favoris
     </PopoverHeader>
     <PopoverBody>
-      Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+
+    {cardWish}
     </PopoverBody>
   </Popover>
     </NavLink>
   </NavItem>
 </Nav>
 <div className='movies'> 
-<h4>Vous avez sélectionné: { likeMovie.join(',') } </h4> 
+<h4>Vous avez sélectionné:  </h4> 
 {movieList}
 </div>
  
